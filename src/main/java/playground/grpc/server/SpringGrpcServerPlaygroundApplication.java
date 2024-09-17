97a7edf82356c21dbe8332a826e54e3152a8bc5b
Mayra Amaral <mayracunha18@gmail.com>
2024-09-17 19:40:34 -0300

    feat: initial structure

---
+++ src/main/java/playground/grpc/server/SpringGrpcServerPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package playground.grpc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcServerPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGrpcServerPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/playground/grpc/server/SpringGrpcServerPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package playground.grpc.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcServerPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
